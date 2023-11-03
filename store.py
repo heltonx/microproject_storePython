0a98f2ee35d90f8b7a1c72ff060abb9e0b304f7b
Helton Xavier Hernandez <52825808+heltonx@users.noreply.github.com>
2023-11-03 20:39:04 -0300

    Add files via upload

---
+++ store.py
@@ -0,0 +1,23 @@
+
+def buy():
+
+    option = '1'
+    total = 0
+    while True:
+        if option == '1':
+            value = int(input("type the value of the product"))
+            total = total + value
+        elif option == '2':
+            break
+        else:
+            print ("opcao invalida")
+        option = input("Keep buying - 1; get out - 2: ")
+
+    print(total)
+
+def main():
+    buy()
+
+if __name__ == "__main__":
+    main()
+    
